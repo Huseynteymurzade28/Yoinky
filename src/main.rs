8d96454d1980e402b3e40c556836df8b551f046b
flear <huseynteymurzade@marun.edu.tr>
2025-07-08 17:08:48 +0300

    add some features

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,30 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 use ratatui::{backend::CrosstermBackend, Terminal};
-use std::io;
+use std::{env, io};
 
 use clap::Parser;
 
 #[derive(Parser)]
 #[command(
     name = "Yoinky",
-    version = "0.1.0",
+    version = env!("CARGO_PKG_VERSION"),
     author = "Huseyn",
     about = "TUI tool for monitoring system resources like CPU, RAM, and GPU."
 )]
 pub struct Cli {
+    #[arg(short, long, help = "Input option for the application")]
     input: Option<String>,
 }
 
 fn main() -> Result<(), io::Error> {
+    // Parse command line arguments
+    let cli = Cli::parse();
+    if let Some(input) = cli.input {
+        println!("Input option provided: {}", input);
+    } else {
+        println!("No input option provided.");
+    }
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
